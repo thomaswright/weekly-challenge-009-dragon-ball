f969c9c44b9816e3b103d42323015ea9eda60819
Thomas Wright <thomaswright1@gmail.com>
2024-07-09 20:40:09 -0600

    update readme

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://thomaswright.github.io/weekly-challenge-009-dragon-ball/",
   plugins: [
     react({
       include: ["**/*.res.mjs"],
